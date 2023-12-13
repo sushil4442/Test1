56cbd2f9f242852565203865322421880e0d8437
Sushil Gaikwad <141930343+sushil4442@users.noreply.github.com>
2023-12-13 19:59:59 +0530

    3rd TC Commit added

---
+++ Scripts/TC_3/Script1702477748162.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser(GlobalVariable.URL)
+
+WebUI.maximizeWindow()
+
+println('Started')
+
+WebUI.takeScreenshot()
+
+WebUI.click(findTestObject('SignUp_Page/Create new account'))
+
+WebUI.verifyElementVisible(findTestObject('SignUp_Page/Its quick and easy'))
+
+WebUI.takeScreenshot()
+
+WebUI.setText(findTestObject('SignUp_Page/FirstName'), GlobalVariable.FName)
+
+WebUI.setText(findTestObject('SignUp_Page/LastName'), GlobalVariable.LName)
+
+WebUI.click(findTestObject('SignUp_Page/button_Sign Up'))
+
+WebUI.takeScreenshot()
+
+WebUI.closeBrowser()
+
